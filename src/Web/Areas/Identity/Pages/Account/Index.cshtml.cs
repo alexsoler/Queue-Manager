460bf6511b74bcb2a31b4e446051d32c629f4600
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-07 12:38:32 -0600

    Page principal para ver, editar y eliminar usuarios

--- src/Web/Areas/Identity/Pages/Account/Index.cshtml.cs
+++ src/Web/Areas/Identity/Pages/Account/Index.cshtml.cs
@@ -17,15 +17,181 @@ namespace Web.Areas.Identity.Pages.Account
     public class IndexModel : PageModel
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public IndexModel(UserManager<ApplicationUser> userManager)
+        public IndexModel(UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
-        public UsuarioViewModel UsuariosRoles { get; private set; } = new UsuarioViewModel();
+        public UsuariosViewModel UsuariosRoles { get; private set; } = new UsuariosViewModel();
+
+        public EditarUsuarioViewModel EditUser { get; set; }
 
         public async Task OnGetAsync()
+        {
+            await LoadUsuarios();
+        }
+
+        public async Task<PartialViewResult> OnGetViewPartialTable()
+        {
+            await LoadUsuarios();
+
+            return PartialView("Account/_TablaUsuarios", UsuariosRoles);
+        }
+
+        public async Task<IActionResult> OnGetViewPartialDetailsAsync(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+                return NotFound();
+
+            var userDetails = await _userManager.FindByNameAsync(username);
+            var roles = await _userManager.GetRolesAsync(userDetails);
+
+            DetallesUsuarioViewModel detalles = new DetallesUsuarioViewModel
+            {
+                Nombre = userDetails.Name,
+                Usuario = userDetails.UserName,
+                Email = userDetails.Email,
+                Telefono = userDetails.PhoneNumber,
+                Roles = roles
+            };
+
+            return PartialView("Account/_DetallesUsuarios", detalles);
+        }
+
+        public async Task<IActionResult> OnGetViewPartialEditAsync(string userName)
+        {
+            if (string.IsNullOrEmpty(userName))
+                return NotFound();
+
+           // ModelState.Clear();
+
+            var user = await _userManager.FindByNameAsync(userName);
+
+            EditUser = new EditarUsuarioViewModel
+            {
+                Name = user.Name,
+                UserName = user.UserName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                Roles = new List<RolesAsignadosData>()
+            };
+
+            foreach (var rol in _roleManager.Roles)
+            {
+                if (await _userManager.IsInRoleAsync(user, rol.Name))
+                    EditUser.Roles.Add(new RolesAsignadosData { Asignado = true, Rol = rol.Name });
+                else
+                    EditUser.Roles.Add(new RolesAsignadosData { Asignado = false, Rol = rol.Name });
+            }
+
+            return PartialView("Account/_EditarUsuario", EditUser);
+        }
+
+        public async Task<IActionResult> OnPostViewPartialEditAsync([FromBody]EditarUsuarioViewModel EditUser)
+        {
+            var user = await _userManager.FindByNameAsync(EditUser.UserName);
+
+            if(user == null)
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+
+            if (user.Name != EditUser.Name)
+            {
+                user.Name = EditUser.Name;
+                await _userManager.UpdateAsync(user);
+            }
+
+            if (user.Email != EditUser.Email)
+            {
+                var setEmailResult = await _userManager.SetEmailAsync(user, EditUser.Email);
+                if (!setEmailResult.Succeeded)
+                {
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    throw new InvalidOperationException($"Unexpected error occurred setting email for user with ID '{userId}'.");
+                }
+            }
+
+            if (user.PhoneNumber != EditUser.PhoneNumber)
+            {
+                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, EditUser.PhoneNumber);
+                if (!setPhoneResult.Succeeded)
+                {
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(EditUser.NewPassword))
+            {
+                var removePasswordResult = await _userManager.RemovePasswordAsync(user);
+                if(!removePasswordResult.Succeeded)
+                {
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    throw new InvalidOperationException($"Unexpected error occurred setting password for user with ID '{userId}'.");
+                }
+
+                var addPasswordResult = await _userManager.AddPasswordAsync(user, EditUser.NewPassword);
+                if (!addPasswordResult.Succeeded)
+                {
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    throw new InvalidOperationException($"Unexpected error occurred setting new password for user with ID '{userId}'.");
+                }
+            }
+
+            foreach (var item in EditUser.Roles)
+            {
+                if(await _userManager.IsInRoleAsync(user, item.Rol))
+                {
+                    if (!item.Asignado)
+                        await _userManager.RemoveFromRoleAsync(user, item.Rol);
+                }
+                else
+                {
+                    if (item.Asignado)
+                        await _userManager.AddToRoleAsync(user, item.Rol);
+                }
+            }
+
+            return new JsonResult("Usuario editado con exito");
+
+        }
+
+        public IActionResult OnGetViewPartialDelete(string username)
+        {
+            return PartialView("_EliminarUsuario", username);
+        }
+
+        public async Task<IActionResult> OnPostViewPartialDeleteAsync(string username)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user == null)
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+
+            var deleteUserResult = await _userManager.DeleteAsync(user);
+            if (!deleteUserResult.Succeeded)
+                throw new InvalidOperationException($"Unexpected error occurred deleting user with UserName '{username}'.");
+
+            return new JsonResult("Usuario eliminado con exito");
+
+        }
+
+        [NonAction]
+        private PartialViewResult PartialView(string nombre, object model)
+        {
+            return new PartialViewResult
+            {
+                ViewName = nombre,
+                ViewData = new ViewDataDictionary<object>(ViewData, model),
+                TempData = this.TempData
+            };
+        }
+
+        [NonAction]
+        private async Task LoadUsuarios()
         {
             UsuariosRoles.Usuarios = await _userManager.Users.ToListAsync();
 
@@ -41,22 +207,6 @@ namespace Web.Areas.Identity.Pages.Account
 
                 UsuariosRoles.Roles.Add(userRoles);
             }
-        }
-
-        public PartialViewResult OnGetViewPartialTable()
-        {
-            return PartialView("Account/_TablaUsuarios", UsuariosRoles);
-        }
-
-        [NonAction]
-        private PartialViewResult PartialView(string nombre, object model)
-        {
-            return new PartialViewResult
-            {
-                ViewName = nombre,
-                ViewData = new ViewDataDictionary<object>(ViewData, model),
-                TempData = this.TempData
-            };
         }
     }
 }
---
+++ src/Web/wwwroot/js/Usuarios.js
@@ -0,0 +1,130 @@
+﻿function EditarUsuario() {
+    let formIsValid  = $("#FormEdit").valid();
+
+    if (formIsValid) {
+        var data = {
+            Name: $("#Name").val(),
+            UserName: $("#UserName").val(),
+            Email: $("#Email").val(),
+            PhoneNumber: $("#PhoneNumber").val(),
+            NewPassword: $("#NewPassword").val(),
+            Roles: $("input[name='Roles']").map(function (index) {
+                return { Rol: this.value, Asignado: this.checked };
+            }).get()
+        };
+
+
+        let jsonData = JSON.stringify(data);
+
+        $.ajax({
+            type: 'POST',
+            url: '/Identity/Account?handler=ViewPartialEdit',
+            data: jsonData,
+            contentType: "application/json",
+            dataType: "json",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("MY-XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val());
+            },
+            success: function (response) {
+                console.log(response);
+                loadTable();
+                loadAlert("El usuario ha sido actualizado con exito.", "Exito", "alert-success");
+            },
+            failure: function () {
+                console.log("No se pudo editar el usuario");
+                loadAlert("No se pudo editar el usuario", "Error", "alert-danger");
+            }
+        });
+
+        $("#editarModal").modal('hide');
+    }
+}
+
+function EliminarUsuario() {
+    let username = $("#deleteUser").val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/Identity/Account?handler=ViewPartialDelete',
+        data: { username },
+        dataType: "json",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("MY-XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val());
+        },
+        success: function (response) {
+            console.log(response);
+            loadTable();
+            loadAlert("El usuario ha sido eliminado con exito", "Exito", "alert-success");
+        },
+        failure: function () {
+            console.log("No se pudo eliminar el usuario");
+            loadAlert("No se pudo eliminar el usuario", "Error", "alert-danger");
+        }
+    });
+
+    $("#eliminarModal").modal('hide');
+}
+
+function loadTable() {
+    $("#divTabla").load('/Identity/Account?handler=ViewPartialTable');
+}
+
+function loadDetails(username) {
+    $.ajax({
+        type: 'GET',
+        url: '/Identity/Account?handler=ViewPartialDetails',
+        data: { username },
+        success: function (response) {
+            $("#bodyDetails").html(response);
+        },
+        failure: function (response) {
+            console.log(response);
+        }
+    });
+}
+
+function loadEdit(username) {
+    $.ajax({
+        type: 'GET',
+        url: '/Identity/Account?handler=ViewPartialEdit',
+        data: { username },
+        success: function (response) {
+            $("#bodyEdit").html(response);
+
+            var form = $('#FormEdit');
+            form.removeData('validator');
+            form.removeData('unobtrusiveValidation');
+            $.validator.unobtrusive.parse(form);
+        },
+        failure: function (response) {
+            console.log(response);
+        }
+    });
+}
+
+function loadDelete(username) {
+    $.ajax({
+        type: 'GET',
+        url: '/Identity/Account?handler=ViewPartialDelete',
+        data: { username },
+        success: function (response) {
+            $("#bodyDelete").html(response);
+        },
+        failure: function (response) {
+            console.log(response);
+        }
+    });
+}
+
+function loadAlert(mensaje, tipoMensaje, nameClass) {
+    $("#cardPrincipalBody").prepend('<div class="alert ' + nameClass + ' alert-dismissible fade show" role="alert">' +
+        '<strong> ' + tipoMensaje + '!</strong > ' +mensaje+
+        '<button class="close" type = "button" data-dismiss="alert" aria-label="Close" >' +
+        '<span aria-hidden="true">×</span>' +
+        '</button>' +
+        '</div>');
+
+    setTimeout(function () {
+        $(".alert").alert('close');
+    }, 5000);
+}
