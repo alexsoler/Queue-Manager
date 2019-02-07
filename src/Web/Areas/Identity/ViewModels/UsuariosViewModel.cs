fe9c3d9a137acb180fc9cb65a1dec51152d8a4f4
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-07 11:03:10 -0600

    Creacion vista parcial para listar los usuarios

---
+++ src/Web/Areas/Identity/ViewModels/UsuariosViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.QueueManager.Infrastructure.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Areas.Identity.ViewModels
+{
+    public class UsuariosViewModel
+    {
+        public IList<ApplicationUser> Usuarios { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
