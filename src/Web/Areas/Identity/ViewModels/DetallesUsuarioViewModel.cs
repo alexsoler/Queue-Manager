7c4c73b7ed2de1cfb40983b9f4edf49f95b283d9
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-07 11:13:26 -0600

    Agregacion de vista parcial y view model para ver detalles de usuarios

---
+++ src/Web/Areas/Identity/ViewModels/DetallesUsuarioViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Areas.Identity.ViewModels
+{
+    public class DetallesUsuarioViewModel
+    {
+        public string Nombre { get; set; }
+        public string Usuario { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string Telefono { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
