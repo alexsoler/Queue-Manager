329d6cb4ef5a706d3564c7319478d75c87322679
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-11 11:55:49 -0600

    Agregacion de queryfilter y borrado suave

--- src/Infrastructure/Identity/AppIdentityDbContext.cs
+++ src/Infrastructure/Identity/AppIdentityDbContext.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +14,24 @@ namespace Microsoft.QueueManager.Infrastructure.Identity
         public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().HasQueryFilter(x => x.Activo == true);
+            base.OnModelCreating(builder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            foreach (var item in ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Deleted &&
+                e.Metadata.GetProperties().Any(x => x.Name == "Activo")))
+            {
+                item.State = EntityState.Unchanged;
+                item.CurrentValues["Activo"] = false;
+            }
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
