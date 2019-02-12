6fb57da035e73e4b1069347637ec47b628ac5b54
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-12 17:42:04 -0600

    Clase abstracta base para las especificaciones

---
+++ src/AplicationCore/Specifications/BaseSpecification.cs
@@ -0,0 +1,53 @@
+﻿using AplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace AplicationCore.Specifications
+{
+    public abstract class BaseSpecification<T> : ISpecification<T>
+    {
+        protected BaseSpecification(Expression<Func<T, bool>> criterio)
+        {
+            Criterio = criterio;
+        }
+
+        public Expression<Func<T, bool>> Criterio { get; }
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+        public List<string> IncludesStrings { get; } = new List<string>();
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
+        public int Take { get; private set; }
+        public int Skip { get; private set; }
+        public bool isPagingEnabled { get; private set; } = false;
+
+        protected virtual void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+
+        protected virtual void AddInclude(string includeString)
+        {
+            IncludesStrings.Add(includeString);
+        }
+
+        protected virtual void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            isPagingEnabled = true;
+        }
+
+        protected virtual void ApplyOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+
+        protected virtual void ApplyOrderByDescending(Expression<Func<T, object>> orderByDescendingExpression)
+        {
+            OrderByDescending = orderByDescendingExpression;
+        }
+    }
+}
