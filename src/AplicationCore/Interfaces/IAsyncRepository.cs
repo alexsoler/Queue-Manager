5096fa92ee56dad61ba67e39bb02c862cdd24050
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-12 17:38:30 -0600

    Creacion de las interfaces de repositorio sincrono y asincrono

---
+++ src/AplicationCore/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,21 @@
+﻿using AplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicationCore.Interfaces
+{
+    public interface IAsyncRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(params object[] keyValues);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+        Task<T> AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+        Task<bool> ExistAsync(ISpecification<T> spec);
+        Task DeleteAsync(T entity);
+        Task<int> CountAsync(ISpecification<T> spec);
+    }
+}
