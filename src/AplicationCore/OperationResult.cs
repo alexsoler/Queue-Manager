16aaf9fe00c935d9fb8078cc7f16e61a542b9760
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-12 17:43:02 -0600

    Clase para conocer el resultado de las operaciones en los servicios

---
+++ src/AplicationCore/OperationResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AplicationCore
+{
+    public class OperationResult
+    {
+        public OperationResult()
+        {
+            Errors = new List<string>();
+        }
+        public bool Succeeded { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
