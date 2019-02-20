b1fdd07558e5944c927ca4d47fabfe4c41cdefed
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-20 11:31:12 -0600

    Agregacion de dos nuevos metodos a la interfaz de office service

--- src/AplicationCore/Interfaces/IOfficeService.cs
+++ src/AplicationCore/Interfaces/IOfficeService.cs
@@ -11,6 +11,8 @@ namespace ApplicationCore.Interfaces
         Task<int> GetOfficesCountAsync(int id);
         Task<int> GetTasksCountAsync(int id);
         Task<int> GetOperatorsCountAsync(int id);
+        Task<bool> HasTask(int idOffice, int idTask);
+        Task<bool> HasOperator(int idOffice, string idOperator);
         Task<Office> GetOfficeAsync(int id);
         Task<IEnumerable<Office>> GetOfficesAsync();
         Task<Office> AddOfficeAsync(Office office);
@@ -21,7 +23,7 @@ namespace ApplicationCore.Interfaces
         Task<IEnumerable<TaskEntity>> GetTasksAsync(int idOffice);
         Task<IEnumerable<ApplicationUser>> GetOperatorsAsync(int idOffice);
         Task<OperationResult> RemoveTaskAsync(int idTask, int idOffice);
-        Task<OperationResult> RemoveOperatorAsync(ApplicationUser applicationUser, int idOffice);
+        Task<OperationResult> RemoveOperatorAsync(string idOperator, int idOffice);
         Task<OperationResult> DeleteOfficeAsync(int id);
         Task<OperationResult> EditOfficeAsync(Office office);
     }
