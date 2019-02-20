b5af55e565bca0826dae643ce59b29de628265e0
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-20 11:45:01 -0600

    Especificacion de query para aplicar paginacion a oficinas y tareas

---
+++ src/AplicationCore/Specifications/OfficesFilterPaginatedSpecification.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Specifications
+{
+    public class OfficesFilterPaginatedSpecification : BaseSpecification<Office>
+    {
+        public OfficesFilterPaginatedSpecification(int skip, int take)
+            : base(null)
+        {
+            ApplyPaging(skip, take);
+        }
+    }
+}
