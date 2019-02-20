862ac3169482e9d1fe331c9601258e3301f83978
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-20 11:48:16 -0600

    Agregacion de view model para paginar oficinas

---
+++ src/Web/ViewModels/OfficeIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.ViewModels
+{
+    public class OfficeIndexViewModel
+    {
+        public IEnumerable<OfficeViewModel> OfficesList { get; set; }
+        public PaginationInfoViewModel PaginationInfo { get; set; }
+    }
+}
