2304715820ea3cb1ac4e1a025601df4e7f8dbedd
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-20 16:16:33 -0600

    Agregacion de View Model para Operador

---
+++ src/Web/ViewModels/OperatorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.ViewModels
+{
+    public class OperatorViewModel
+    {
+        public string Id { get; set; }
+        [Display(Name = "Nombre:")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; }
+    }
+}
