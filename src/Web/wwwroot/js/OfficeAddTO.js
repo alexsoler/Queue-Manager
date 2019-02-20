1b64c8a8378f7ad71707f41003f4f5838aea8155
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-20 12:05:01 -0600

    Agregacion de vista y scripts para agregar operadores y tareas a una nueva oficina

---
+++ src/Web/wwwroot/js/OfficeAddTO.js
@@ -0,0 +1,32 @@
+﻿function Guardar() {
+
+    office.OfficeTasks = $("input[name='Tareas']").map(function () {
+        if (this.checked) {
+            return { OfficeId: office.Id, TaskId: this.value };
+        }
+    }).get();
+
+    office.OfficeOperators = $("input[name='Operadores']").map(function () {
+        if (this.checked) {
+            return { OfficeId: office.Id, ApplicationUserId: this.value };
+        }
+    }).get();
+
+    $.ajax({
+        type: 'POST',
+        url: '/Offices/TaskAndOperatorsSave',
+        data: { officevm: office },
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("MY-XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val());
+        }
+    }).done(function (result) {
+        console.log(result);
+        window.location = "/Offices/Index";
+    }).fail(function (result) {
+        console.log(result);
+    });
+}
+
+function Cancelar() {
+    window.location = "/";
+}
