4bd2cbc52ed66ec551ec77e8e0fe89928ca20ad8
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-20 11:42:36 -0600

    Modificacion de la especificacion de query para Office

--- src/AplicationCore/Specifications/OfficeSpecification.cs
+++ src/AplicationCore/Specifications/OfficeSpecification.cs
@@ -16,5 +16,18 @@ namespace ApplicationCore.Specifications
             : base(x => x.Id == idOffice)
         {
         }
+
+        public OfficeSpecification(int idOffice, bool withTasks = false, bool withOperators = false)
+            :base(x => x.Id == idOffice)
+        {
+            if(withTasks)
+            {
+                AddInclude(x => x.OfficeTasks);
+            }
+            if(withOperators)
+            {
+                AddInclude(x => x.OfficeOperators);
+            }
+        }
     }
 }
