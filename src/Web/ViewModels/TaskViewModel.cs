5394202de2f0eb28ce18f32876508bd18c1da599
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-02-20 11:49:25 -0600

    Agregacion de view model para tareas

---
+++ src/Web/ViewModels/TaskViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.ViewModels
+{
+    public class TaskViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Nombre:")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; }
+        [Display(Name = "Fecha de creación")]
+        [DataType(DataType.DateTime)]
+        public DateTime CreationDate { get; set; }
+    }
+}
