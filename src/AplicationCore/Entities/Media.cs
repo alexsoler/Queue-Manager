b422476eeff96a8327e426652af8aa23f1af0e58
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-03-02 15:30:39 -0600

    Entidad, servicio y especificaciones para media

---
+++ src/AplicationCore/Entities/Media.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class Media : BaseEntity
+    {
+        public int Id { get; set; }
+        public bool Video { get; set; }
+        public bool Audio { get; set; }
+        public bool Img { get; set; }
+        public bool Used { get; set; }
+        public string Name { get; set; }
+        public byte[] File { get; set; }
+    }
+}
