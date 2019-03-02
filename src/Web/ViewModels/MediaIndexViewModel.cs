1846b7c25cde0b8f67a1cc6e15eda5a9a8b2d939
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-03-02 15:32:39 -0600

    ViewModels for Media

---
+++ src/Web/ViewModels/MediaIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.ViewModels
+{
+    public class MediaIndexViewModel
+    {
+        public IEnumerable<MediaViewModel> MediaList { get; set; }
+        public PaginationInfoViewModel PaginationInfo { get; set; }
+    }
+}
