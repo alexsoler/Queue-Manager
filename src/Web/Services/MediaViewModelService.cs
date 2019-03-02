b24f8aa7aef2df6ac1a70380f15231ff60d9e45c
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-03-02 15:37:25 -0600

    Service MediaViewModel

---
+++ src/Web/Services/MediaViewModelService.cs
@@ -0,0 +1,59 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Specifications;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Interfaces;
+using Web.ViewModels;
+
+namespace Web.Services
+{
+    public class MediaViewModelService : IMediaViewModel
+    {
+        private readonly IRepository<Media> _repository;
+        private readonly IAppLogger<MediaViewModelService> _logger;
+        private readonly IMapper _mapper;
+
+        public MediaViewModelService(IRepository<Media> repository,
+            IAppLogger<MediaViewModelService> logger,
+            IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        public MediaIndexViewModel GetMediasPagination(int pageIndex, int itemsPage)
+        {
+            var filterPaginatedSpecification =
+                new MediasFilterPaginatedSpecification(itemsPage * (pageIndex - 1), itemsPage);
+
+            var mediasOnPage = _mapper.Map<IEnumerable<Media>, IEnumerable<MediaViewModel>>(
+                _repository.ListAll());
+
+            var totalMedias = _repository.Count(new MediaSpecification());
+
+            var vm = new MediaIndexViewModel
+            {
+                MediaList = mediasOnPage,
+                PaginationInfo = new PaginationInfoViewModel
+                {
+                    ActualPage = totalMedias,
+                    ItemsPerPage = itemsPage,
+                    TotalItems = totalMedias,
+                    TotalPages = int.Parse(Math.Ceiling((decimal)totalMedias / itemsPage).ToString())
+                }
+            };
+
+            vm.PaginationInfo.Controller = "Medias";
+            vm.PaginationInfo.Action = "Table";
+            vm.PaginationInfo.Next = (vm.PaginationInfo.ActualPage == vm.PaginationInfo.TotalPages) ? "disabled" : "";
+            vm.PaginationInfo.Previous = (vm.PaginationInfo.ActualPage == 1) ? "disabled" : "";
+
+            return vm;
+        }
+    }
+}
