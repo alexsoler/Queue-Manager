a1904d1db468b03de6881b8d38cd299183a43355
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-03-08 11:21:48 -0600

    Controller medias finish

--- src/Web/Controllers/MediasController.cs
+++ src/Web/Controllers/MediasController.cs
@@ -1,26 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using ApplicationCore.Entities;
 using ApplicationCore.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Web.Interfaces;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
+    [Authorize(Roles = "Administrador")]
     public class MediasController : Controller
     {
         private readonly IMediaService _mediaService;
         private readonly IMediaViewModel _mediaViewModelService;
+        private readonly IMapper _mapper;
         private readonly IAppLogger<MediasController> _logger;
 
         public MediasController(IMediaService mediaService,
             IMediaViewModel mediaViewModelService,
-            IAppLogger<MediasController> logger)
+            IAppLogger<MediasController> logger,
+            IMapper mapper)
         {
             _mediaService = mediaService;
             _mediaViewModelService = mediaViewModelService;
+            _mapper = mapper;
             _logger = logger;
         }
 
@@ -30,88 +39,90 @@ namespace Web.Controllers
             return View();
         }
 
+        // GET: Medias/Table?pag=5
         [ActionName("Table")]
         public PartialViewResult PartialViewTable(int? pag)
         {
             var itemsPage = 5;
             var mediaIndexModel = _mediaViewModelService.GetMediasPagination(pag ?? 1, itemsPage);
 
-            return PartialView("_TablaMultimedia", mediaIndexModel);
+            return PartialView("_TableAndPagination", mediaIndexModel);
         }
 
-        // GET: Medias/Details/5
-        public ActionResult Details(int id)
+        // GET: Medias/GetMedia
+        public async Task<IActionResult> GetMedia(int? id)
         {
-            return View();
-        }
+            if (!id.HasValue)
+                return NotFound();
 
-        // GET: Medias/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+            var file = await _mediaService.GetMediaAsync(id.Value);
 
-        // POST: Medias/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Medias/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Medias/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return File(file.File, file.ContentType);
         }
 
         // GET: Medias/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int? id)
         {
-            return View();
+            if (!id.HasValue)
+                return Conflict("No se recibio el id");
+
+
+            var media = await _mediaService.GetMediaAsync(id.Value);
+
+            if (media == null)
+                return NotFound();
+
+            var mediavm = _mapper.Map<MediaViewModel>(media);
+
+            return PartialView("_EliminarMedia", mediavm);
         }
 
         // POST: Medias/Delete/5
         [HttpPost]
+        [ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> DeleteMedia(int? id)
         {
+            if (!id.HasValue)
+                return Conflict("No se recibio el id");
+
             try
             {
                 // TODO: Add delete logic here
+                var result = await _mediaService.RemoveAsync(id.Value);
 
-                return RedirectToAction(nameof(Index));
+                _logger.LogInformation($"Se elimino un archivo de id {id.Value}");
+                return Ok(result);
             }
             catch
             {
-                return View();
+                _logger.LogInformation($"No se pudo eliminar el archivo de id {id.Value}");
+                return Conflict("No se pudo elimimar el archivo");
             }
+        }
+
+        [HttpPost]
+        [DisableRequestSizeLimit]
+        [RequestFormLimits(MultipartBodyLengthLimit = int.MaxValue)]
+        public async Task<IActionResult> UploadFiles(IList<IFormFile> files)
+        {
+            _logger.LogInformation($"Se almacenaran {files.Count} archivos multimedia.");
+
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var media = _mapper.Map<Media>(formFile);
+
+                    await _mediaService.AddMediaAsync(media);
+                }
+            }
+
+            _logger.LogInformation($"Se alamacenaron {files.Count} archivos multimedia. Tamaño: {size} ");
+
+            return Ok(new { count = files.Count, size });
         }
     }
 }
