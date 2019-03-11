fed4da9be100467320dc50f9525d4d0441a022ba
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-03-11 09:54:39 -0600

    Add Touch Controller

---
+++ src/Web/Controllers/TouchController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    public class TouchController : Controller
+    {
+        private readonly ITaskService _taskService;
+        private readonly IMapper _mapper;
+        private readonly IRepository<Priority> _repositoryPriority;
+        private readonly ILogger<TouchController> _logger;
+
+        public TouchController(ITaskService taskService,
+            IMapper mapper,
+            IRepository<Priority> repository,
+            ILogger<TouchController> logger)
+        {
+            _taskService = taskService;
+            _mapper = mapper;
+            _repositoryPriority = repository;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var vm = new TouchViewModel();
+
+            var tasks = await _taskService.GetAllAsync();
+
+            vm.Tasks = _mapper.Map<IEnumerable<TaskEntity>, List<TaskViewModel>>(tasks);
+            vm.Priorities = _repositoryPriority.ListAll().ToList();
+
+            return View(vm);
+        }
+    }
+}
