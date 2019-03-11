151f02fb033f430a74f2c79195b485919cfc8500
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-03-11 09:46:57 -0600

    Add Entity Ticket

---
+++ src/AplicationCore/Entities/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class Ticket
+    {
+        public long Id { get; set; }
+        public int OfficeId { get; set; }
+        public int TaskEntityId { get; set; }
+        public string ApplicationUserId { get; set; }
+        public int StatusId { get; set; }
+        public int PriorityId { get; set; }
+        public DateTime InitialWaitDate { get; set; }
+        public DateTime InitialAttentionDate { get; set; }
+        public DateTime CompletionAttentionDate { get; set; }
+        public int NumberTicket { get; set; }
+        public string DisplayTokenName { get; set; }
+    }
+}
