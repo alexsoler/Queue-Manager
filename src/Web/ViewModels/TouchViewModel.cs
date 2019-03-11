2a8acf96a011465ad86c3778c248c4049bf53464
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-03-11 09:53:44 -0600

    Add touchViewModel

---
+++ src/Web/ViewModels/TouchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.ViewModels
+{
+    public class TouchViewModel
+    {
+        public List<TaskViewModel> Tasks { get; set; }
+        public List<Priority> Priorities { get; set; }
+    }
+}
