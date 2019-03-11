4362fa3ce2556026aaf24b5e95d7b231aa33325f
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-03-11 09:58:02 -0600

    Add view script and style for Touch

---
+++ src/Web/wwwroot/js/Touch.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+var tarea, prioridad;
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/queueHub").build();
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+function openNav(idTarea) {
+    document.getElementById("myNav").style.height = "100%";
+    tarea = idTarea;
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+function crearToken(idPrioridad) {
+    prioridad = idPrioridad;
+    console.log(tarea, prioridad);
+
+    connection.invoke("CreateToken", { IdTarea: tarea, IdPrioridad: prioridad })
+        .catch(function (err) {
+            return console.error(err.toString());
+        });
+
+    event.preventDefault();
+}
+
