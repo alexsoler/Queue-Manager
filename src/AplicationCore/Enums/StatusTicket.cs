b0f2d8bde957a54b58aca6c06641b76db9f26099
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-03-11 19:01:23 -0600

    Add Enum Status Ticket

---
+++ src/AplicationCore/Enums/StatusTicket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Enums
+{
+    public enum StatusTicket
+    {
+        OnHold = 1,
+        InAssistance = 2,
+        Processed = 3,
+        NotProcessed = 4
+    }
+}
