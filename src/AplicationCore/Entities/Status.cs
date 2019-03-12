c9f4ae1905e1449822dc7749fb392d285a446c7d
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-03-11 18:59:20 -0600

    Edit Entity Status

--- src/AplicationCore/Entities/Status.cs
+++ src/AplicationCore/Entities/Status.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ApplicationCore.Entities
 {
-    public class Status
+    public class Status : BaseEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
