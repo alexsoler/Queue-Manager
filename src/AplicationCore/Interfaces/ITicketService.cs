ea94c3eb129765114e743b95dc70afa9db2e81be
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-03-12 19:45:41 -0600

    Add Ticket service

--- src/AplicationCore/Interfaces/ITicketService.cs
+++ src/AplicationCore/Interfaces/ITicketService.cs
@@ -1,4 +1,5 @@
 ﻿using ApplicationCore.Entities;
+using ApplicationCore.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,8 @@ namespace ApplicationCore.Interfaces
 {
     public interface ITicketService
     {
-        Task<Ticket> CreateNewTicket(int idTask, int idPriority); 
+        Task<Ticket> CreateNewTicket(int idTask, int idPriority);
+        Task<IEnumerable<Ticket>> GetTickets(StatusTicket statusTicket, int idOffice);
+        Task<IReadOnlyList<string>> GetOfficesTask(int idTask);
     }
 }
--- src/AplicationCore/Services/TicketService.cs
+++ src/AplicationCore/Services/TicketService.cs
@@ -4,6 +4,7 @@ using ApplicationCore.Interfaces;
 using ApplicationCore.Specifications;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,14 +14,17 @@ namespace ApplicationCore.Services
     {
         private readonly IAsyncRepository<Ticket> _asyncRepository;
         private readonly IAsyncRepository<TaskEntity> _taskRepository;
+        private readonly IAsyncRepository<OfficeTask> _officeTaskRepository;
         private readonly IAppLogger<TicketService> _logger;
 
         public TicketService(IAsyncRepository<Ticket> asyncRepository,
             IAsyncRepository<TaskEntity> taskRepository,
+            IAsyncRepository<OfficeTask> officeTaskRepository,
             IAppLogger<TicketService> logger)
         {
             _asyncRepository = asyncRepository;
             _taskRepository = taskRepository;
+            _officeTaskRepository = officeTaskRepository;
             _logger = logger;
         }
 
@@ -29,14 +33,14 @@ namespace ApplicationCore.Services
             _logger.LogInformation("Creando un nuevo ticket");
             int numberTicket = 0;
 
-            var specificationTicket = new TicketSpecification(StatusTicket.OnHold);
+            var specificationTicket = new TicketSpecification(StatusTicket.OnHold, OrderByDescending: true);
             var lastTicket = await _asyncRepository.GetSingleBySpecAsync(specificationTicket);
 
             var task = await _taskRepository.GetByIdAsync(idTask);
 
             if (lastTicket != null)
             {
-                if(lastTicket.NumberTicket < 999)
+                if (lastTicket.NumberTicket < 999)
                     numberTicket = lastTicket.NumberTicket;
             }
 
@@ -57,7 +61,25 @@ namespace ApplicationCore.Services
             newTicket = await _asyncRepository.GetSingleBySpecAsync(new TicketSpecification(newTicket.Id));
 
             return newTicket;
-            
+
+        }
+
+        public async Task<IEnumerable<Ticket>> GetTickets(StatusTicket statusTicket, int idOffice)
+        {
+            var officeTasks = await _officeTaskRepository.ListAsync(new OfficeWithTaskSpecification(idOffice, includeTask: true));
+
+            var tasks = officeTasks.Select(x => x.Task).ToList();
+
+            var specificationTicket = new TicketSpecification(statusTicket, tasks);
+
+            return await _asyncRepository.ListAsync(specificationTicket);
+        }
+
+        public async Task<IReadOnlyList<string>> GetOfficesTask(int idTask)
+        {
+            var taskOffices = await _officeTaskRepository.ListAsync(new TaskWithOffices(idTask, includeOffice: true));
+
+            return taskOffices.Select(x => x.Office.Id.ToString()).ToList();
         }
     }
 }
