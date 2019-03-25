35823bb6e013e324338e917704920a67f0c0d67b
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-03-25 15:57:32 -0600

    Add model DisplayTickets

---
+++ src/Web/Models/DisplayTickets.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class DisplayTickets
+    {
+        public string ActualTicket { get; set; }
+        public string ActualOffice { get; set; }
+        public string TicketH1 { get; set; }
+        public string OfficeH1 { get; set; }
+        public string TicketH2 { get; set; }
+        public string OfficeH2 { get; set; }
+        public string TicketH3 { get; set; }
+        public string OfficeH3 { get; set; }
+    }
+}
