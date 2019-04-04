a969826fe4fa999557c5b1d99b354ee077d9ab72
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-04-04 16:18:03 -0600

    Add controller, views  for Custom Display

---
+++ src/Web/Controllers/CustomDisplayController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    public class CustomDisplayController : Controller
+    {
+        private readonly IMediaService _mediaService;
+        private readonly IDisplayMediaService _displayMediaService;
+        private readonly IMapper _mapper;
+
+        public CustomDisplayController(IMediaService mediaService,
+            IDisplayMediaService displayMediaService,
+            IMapper mapper)
+        {
+            _mediaService = mediaService;
+            _displayMediaService = displayMediaService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var mediaList = await _mediaService.GetAllMediaNotUsedAsync();
+            var displayMedia = await _displayMediaService.ListAllAsync();
+
+            ViewData["listMedia"] = _mapper.Map<IEnumerable<MediaViewModel>>(mediaList);
+            ViewData["listDisplayMedia"] = _mapper.Map<IEnumerable<DisplayMediaViewModel>>(displayMedia);
+
+            return View();
+        }
+
+        public async Task<ActionResult> DisplayMediaPV()
+        {
+            var displayMedia = await _displayMediaService.ListAllAsync();
+            var listDisplayMedia = _mapper.Map<IEnumerable<DisplayMediaViewModel>>(displayMedia);
+
+            return PartialView("_listDisplay", listDisplayMedia);
+        }
+
+        public async Task<ActionResult> MediaPV()
+        {
+            var mediaList = await _mediaService.GetAllMediaNotUsedAsync();
+            var listMedia = _mapper.Map<IEnumerable<MediaViewModel>>(mediaList);
+
+            return PartialView("_listMedia", listMedia);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMediaToDisplay(int? id, double? order)
+        {
+            if (!id.HasValue || !order.HasValue)
+                return BadRequest();
+
+            var displayMedia = new DisplayMedia
+            {
+                MediaId = id.Value,
+                Order = order.Value,
+                CreationDate = DateTime.Now
+            };
+
+            await _displayMediaService.AddAsync(displayMedia);
+            await _mediaService.SetAsUsed(id.Value);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveMediaToDisplay(int? id, int? idMedia)
+        {
+            if (!id.HasValue || !idMedia.HasValue)
+                return BadRequest();
+
+            await _displayMediaService.RemoveAsync(id.Value);
+            await _mediaService.SetAsNotUsed(idMedia.Value);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateOrder(int? id, double? order)
+        {
+            if (!id.HasValue || !order.HasValue)
+                return BadRequest();
+
+            await _displayMediaService.UpdateOrder(id.Value, order.Value);
+
+            return Ok();
+        }
+    }
+}
---
+++ src/Web/wwwroot/js/CustomDisplay.js
@@ -0,0 +1,79 @@
+﻿function AddMediaToDisplay(id, e) {
+    const $LisDisplay = $("#listDisplay li");
+    let order = 100;
+    if ($LisDisplay.length > 0) {
+        order = $LisDisplay.last().data("order") + 100;
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: '/CustomDisplay/AddMediaToDisplay',
+        data: { id, order }
+    }).done((result) => {
+        loadListDisplayMedia();
+        $(e.parentElement).fadeOut("fast", (element) => $(element).remove());
+    }).fail((result) => {
+        console.error(result);
+    });
+}
+
+function RemoveMediaToDisplay(id, idMedia, e) {
+    $.ajax({
+        type: 'POST',
+        url: '/CustomDisplay/RemoveMediaToDisplay',
+        data: { id, idMedia }
+    }).done(() => {
+        loadListMedia();
+        $(e.parentElement).fadeOut("fast", (element) => $(element).remove());
+    }).fail((result) => {
+        console.error(result);
+    });
+}
+
+function loadListDisplayMedia() {
+    $("#divLoadDisplay").load("/CustomDisplay/DisplayMediaPV", function () {
+        ps1 = new PerfectScrollbar("#listDisplay");
+        AddSortable();
+    });
+}
+
+function loadListMedia() {
+    $("#divLoadMedia").load("/CustomDisplay/MediaPV", function () {
+        ps2 = new PerfectScrollbar("#listMedia");
+    });
+}
+
+function AddSortable() {
+    $("#listDisplay").sortable({
+        update: function (event, ui) {
+            const id = ui.item.data("id");
+            let prevOrder = ui.item.prev().data("order");
+            let nextOrder = ui.item.next().data("order");
+            let order = 0;
+
+            if (prevOrder === undefined) {
+                prevOrder = 0;
+            }
+
+            if (nextOrder === undefined) {
+                order = Number(prevOrder) + 100;
+            }
+            else {
+                order = (Number(prevOrder) + Number(nextOrder)) / 2;
+            }
+
+            $.ajax({
+                type: 'POST',
+                url: '/CustomDisplay/UpdateOrder',
+                data: { id, order }
+            }).done(() => {
+                console.log('Nuevo orden aplicado');
+                ui.item.data("order", order);
+            }).fail((error) => {
+                console.error('No se pudo aplicar un nuevo orden: ' + error);
+            });
+        }
+    });
+}
+
+AddSortable();
