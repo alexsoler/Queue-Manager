cb6b00086f5c5da877c3b9d260cacd7ec43687ab
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-04-04 16:04:35 -0600

    Add DisplayMedia Service

---
+++ src/AplicationCore/Interfaces/IDisplayMediaService.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IDisplayMediaService
+    {
+        Task<DisplayMedia> AddAsync(DisplayMedia displayMedia);
+        Task<IEnumerable<DisplayMedia>> ListAllAsync();
+        Task<OperationResult> RemoveAsync(int id);
+
+        Task<OperationResult> UpdateOrder(int id, double order);
+    }
+}
