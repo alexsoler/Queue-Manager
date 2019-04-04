87d158304ecf4ed0dc2bd0e394523e3d1254d022
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-04-04 16:11:06 -0600

    Add DisplayMediaViewModel

---
+++ src/Web/ViewModels/DisplayMediaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.ViewModels
+{
+    public class DisplayMediaViewModel
+    {
+        public int Id { get; set; }
+        public double Order { get; set; }
+        public string Nombre { get; set; }
+        public int IdMedia { get; set; }
+        public string Tipo { get; set; }
+    }
+}
