34a85bc29b42834e345f788de5be337451adfd7d
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-04-04 16:07:29 -0600

    Add displayMediaSpecification

---
+++ src/AplicationCore/Specifications/DisplayMediaSpecification.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Specifications
+{
+    public class DisplayMediaSpecification : BaseSpecification<DisplayMedia>
+    {
+        public DisplayMediaSpecification() : base(null)
+        {
+            AddInclude(x => x.Media);
+            ApplyOrderBy(x => x.Order);
+        }
+    }
+}
